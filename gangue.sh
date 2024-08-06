76c672d43f5e2aab9f76f228076c43b25ce2682a
RobertLlb <robertlacerda95@hotmail.com>
2024-08-06 09:49:46 -0300

    new files
    update

---
+++ gangue.sh
@@ -0,0 +1,13 @@
+wget https://raw.githubusercontent.com/formigapop/ore/main/install_ore.sh -O install_ore.sh
+
+chmod +x install_ore.sh
+
+wget https://raw.githubusercontent.com/formigapop/ore/main/run_ore.sh -O run_ore.sh
+
+chmod +x run_ore.sh
+
+wget https://raw.githubusercontent.com/formigapop/ore/main/stop_ore.sh -O stop_ore.sh
+
+chmod +x stop_ore.sh
+
+sh install_ore.sh
